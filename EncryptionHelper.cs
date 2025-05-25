ae9b3412a4ffe5761674196c8533afce1e17d985
Adrian Bailador <adrianbailador@hotmail.com>
2025-05-25 12:45:35 +0100

    EncryptionHelper

---
+++ EncryptionHelper.cs
@@ -0,0 +1,47 @@
+using System.Security.Cryptography;
+using System.Text;
+
+public class EncryptionHelper
+{
+    private readonly byte[] _key;
+    private readonly byte[] _iv;
+
+    public EncryptionHelper(IConfiguration config)
+    {
+        _key = Convert.FromBase64String(config["Encryption:Key"]!);
+        _iv = Convert.FromBase64String(config["Encryption:IV"]!);
+    }
+
+    public string Encrypt(string plainText)
+    {
+        using var aes = Aes.Create();
+        aes.Key = _key;
+        aes.IV = _iv;
+
+        using var encryptor = aes.CreateEncryptor();
+        using var ms = new MemoryStream();
+        using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+        using (var writer = new StreamWriter(cs))
+        {
+            writer.Write(plainText);
+        }
+
+        return Convert.ToBase64String(ms.ToArray());
+    }
+
+    public string Decrypt(string cipherText)
+    {
+        var buffer = Convert.FromBase64String(cipherText);
+
+        using var aes = Aes.Create();
+        aes.Key = _key;
+        aes.IV = _iv;
+
+        using var decryptor = aes.CreateDecryptor();
+        using var ms = new MemoryStream(buffer);
+        using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+        using var reader = new StreamReader(cs);
+
+        return reader.ReadToEnd();
+    }
+}
